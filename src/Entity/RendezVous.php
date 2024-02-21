19d4bf9f6ff242b50b237299ab18a8c6c0d45daf
HasnaouiOumayma <oumayma.hasnaoui@esprit.tn>
2024-02-21 19:50:01 +0100

    crud de deux entités avec controle de saisie (back et front)19/02/2024

--- src/Entity/RendezVous.php
+++ src/Entity/RendezVous.php
@@ -53,17 +53,7 @@ class RendezVous
     {
         $this->consultation = $consultation;
     }
-    private ?cnam $cnam = null;
 
-    public function getcnam(): ?cnam
-    {
-        return $this->cnam;
-    }
-
-    public function setcnam(?cnam $cnam): void
-    {
-        $this->cnam = $cnam;
-    }
 
 
     public function getId(): ?int
